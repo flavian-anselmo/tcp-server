b0c59126604280f89f4ebcfd461b106b72db9bdc
flavian-anselmo <anselmoflavian4@gmail.com>
2023-02-05 16:06:06 +0300

    tcp-server v1.0 test 🚀

--- prep/client.py
+++ prep/client.py
@@ -1,17 +1,57 @@
 import socket 
 
-c = socket.socket()
+HEADER = 64 
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONECT_MSG = 'd'
+IP_ADD =  '192.168.8.6'
+ADDR = (IP_ADD, PORT)
 
-#conect to a server 
-c.connect(
-    ('localhost', 9998)
+#create a socket 
+client = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
 )
 
-name = input('Enter name: ')
+#make the connection 
+client.connect((IP_ADD, PORT))
 
-#send data to the server 
-c.send(bytes(name, 'utf-8'))
 
-# recieve data from the server 
-r = c.recv(1024).decode()
+def send(msg):
+    '''
+    send a message to the server 
+
+    '''
+    #en code the message 
+    message = msg.encode(FORMAT)
+    msg_len = len(message)
+    send_len = str(msg_len).encode(FORMAT)
+    
+    #padding 
+    send_len += b' ' * (HEADER - len(send_len))
+    
+    client.send(send_len)
+    client.send(message)
+def recieve_msg_from_server():
+    '''
+    recieve mmsg from the server 
+
+    '''
+    r_msg = client.recv(HEADER).decode()
+    return r_msg
+
+send(DISCONECT_MSG)
+
+r = recieve_msg_from_server()
 print(r)
+
+
+
+
+
+
+
+
+
+
+
--- prep/server.py
+++ prep/server.py
@@ -1,41 +1,92 @@
 import socket
+import threading
 
-PORT = 9998
-IP_ADD = 'localhost'
+PORT = 5050
+IP_ADD =  socket.gethostbyname(socket.gethostname())
 N = 3 
+FORMAT = 'utf-8'
+HEADER = 64
+DISCONNECT_MSG = 'd'
 
-sk = socket.socket(
+server = socket.socket(
     socket.AF_INET,   
     socket.SOCK_STREAM, 
+)
+
+addr =  (
+    IP_ADD,
+    PORT,
 )
 
 print('socket created...')
 
 
 # bid a socket with a port number 
-sk.bind(
-    (
-        IP_ADD,
-        PORT,
-    )
-)
+server.bind(addr)
 
 
-#listen to a client 
-sk.listen(
-    N
-)
-print('waiting for connections...')
 
+def handle_clients(conn, addr):
+    '''
+    - handle the cients :thread 
+    - handle the inidvitual connection 
+    - concurrently for each client 
 
-while True:
-    # continnous connection 
-    c_socket, addr = sk.accept()
-    name = c_socket.recv(1024).decode()
-    print('connected with', addr, name)
+    '''
+    print(f'[NEW CONNCETION] {addr} connected')
+    connected = True
+    while connected:
+
+        #recieve the message 
+        msg_len = conn.recv(HEADER).decode(FORMAT)
+
+        # if the message is not null 
+        if msg_len:
+            msg_len = int(msg_len)
+            print(msg_len)
+            msg = conn.recv(msg_len).decode(FORMAT)
+            print(msg)
+
+            # disconnection from the server 
+            if msg == DISCONNECT_MSG:
+                # close the current connection 
+                conn.send(
+                    bytes(f'diconnected client {addr}', FORMAT)
+                )
+                connected = False
+            else:
+                conn.send(
+                    bytes(f'|<---->|WELCOME|<---->|', FORMAT)
+                )
+                print(f'[{addr}]: {msg}')
+    #close the connection 
+    conn.close()
+
+def start_server():
+    '''
+    - start the tcp server 
+    - create a thread in the python process 
     
-    c_socket.send(
-        bytes(f'welcome', 'utf-8')
+    '''
+    #listen to a client 
+    server.listen(
+        N
     )
-    c_socket.close()
 
+    print(f'[LISTENING]--> server is listenng on {IP_ADD}')
+    
+    while True:
+        # continnous connection 
+        c_socket, addr = server.accept()
+
+        # create a thread
+        client_thread = threading.Thread(target = handle_clients, args = (c_socket, addr)) 
+        
+        #start the thread 
+        client_thread.start()
+
+        #print the active threads in this python process 
+        print(f'[ACTIVE CONNECTIONS] {threading.active_count()-1}')
+
+#START_THE_SERVER 
+start_server()
